1bea7cc4f6a9fd138acee2f45ce53dfdfd6d31e9
ReadnThink <103480627+ReadnThink@users.noreply.github.com>
2023-12-02 23:14:47 +0900

    sevlet req, resp를 담당하는 ServletResponseDto Util적용

--- src/main/java/com/jsp/board/servlet/GugudanServlet.java
+++ src/main/java/com/jsp/board/servlet/GugudanServlet.java
@@ -1,5 +1,6 @@
 package com.jsp.board.servlet;
 
+import com.jsp.board.util.ServletResponseDto;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -12,16 +13,14 @@ import java.io.IOException;
 public class GugudanServlet extends HttpServlet {
     @Override
     protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
-        req.setCharacterEncoding("UTF-8"); //들어오는 데이터를 UTF-8로 해석하겠다.
-        resp.setCharacterEncoding("UTF-8"); // 반환하는 HTML의 인코딩의 UTF-8로 하겠다.
-        resp.setContentType("text/html; charset-utf-8"); // 브라우저에게 우리가 만든 결과물이 UTF-8이라고 알려줌
+        ServletResponseDto servletResponseDto = new ServletResponseDto(req, resp);
 
-        int dan = Integer.parseInt(req.getParameter("dan"));
-        int limit = Integer.parseInt(req.getParameter("limit"));
+        int dan = servletResponseDto.getIntParam(("dan"), 9);
+        int limit = servletResponseDto.getIntParam(("limit"), 9);
 
-        resp.getWriter().append("<h1>%d단</h1>".formatted(dan));
+        servletResponseDto.appendBody("<h1>%d단</h1>\n".formatted(dan));
         for (int i = 1; i <= limit; i++) {
-            resp.getWriter().append("<div>%d * %d = %d <div>\n".formatted(dan, i, dan * i));
+            servletResponseDto.appendBody(("<div>%d * %d = %d <div>\n".formatted(dan, i, dan * i)));
         }
     }
 }
---
+++ src/main/java/com/jsp/board/util/ServletResponseDto.java
@@ -0,0 +1,58 @@
+package com.jsp.board.util;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+public class ServletResponseDto {
+    final HttpServletRequest req;
+    final HttpServletResponse resp;
+
+    public ServletResponseDto(final HttpServletRequest req, final HttpServletResponse resp) {
+        this.req = req;
+        this.resp = resp;
+
+        try {
+            req.setCharacterEncoding("UTF-8"); //들어오는 데이터를 UTF-8로 해석하겠다.
+            resp.setCharacterEncoding("UTF-8"); // 반환하는 HTML의 인코딩의 UTF-8로 하겠다.
+            resp.setContentType("text/html; charset-utf-8"); // 브라우저에게 우리가 만든 결과물이 UTF-8이라고 알려줌
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public int getIntParam(String key, int defaultValue) {
+        String value = req.getParameter(key);
+
+        if (value == null) {
+            return defaultValue;
+        }
+
+        try {
+            return Integer.parseInt(value);
+        } catch (NullPointerException e) {
+            return defaultValue;
+        }
+    }
+
+    public String getParam(String key, String defaultValue) {
+        final String value = req.getParameter(key);
+
+        if (value == null) {
+            return defaultValue;
+        }
+
+        return value;
+    }
+
+    public void appendBody(String str) {
+        try {
+            resp.getWriter().append(str);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
