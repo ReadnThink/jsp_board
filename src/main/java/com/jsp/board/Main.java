0a3966e14efafd3df15decbeec365542f9d19faf
ReadnThink <103480627+ReadnThink@users.noreply.github.com>
2023-12-02 19:49:44 +0900

    tomcat, servlet 설정 및 테스트

--- src/main/java/com/jsp/board/Main.java
+++ src/main/java/com/jsp/board/Main.java
@@ -2,6 +2,5 @@ package com.jsp.board;
 
 public class Main {
     public static void main(String[] args) {
-
     }
 }
---
+++ src/main/java/com/jsp/board/servlet/HelloServlet.java
@@ -0,0 +1,18 @@
+package com.jsp.board.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().append("Hello!");
+    }
+}
